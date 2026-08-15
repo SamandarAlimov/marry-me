49ce26698e05aaa2cfe53ae6f20eb0a43875950a
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-08-16 00:56:29 +0500

    style: load planner interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles.css'
 import './hero-garden.css'
+import './styles/planner.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
