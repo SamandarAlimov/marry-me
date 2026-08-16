ad7f99307db5a57a0632fab3996b6bee0c9b35f2
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-08-16 08:35:27 +0500

    style: load living atmosphere styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App'
 import './styles.css'
 import './hero-garden.css'
 import './styles/planner.css'
+import './styles/living-atmosphere.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
